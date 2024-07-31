21f8a1d1b7006be6d13eba77ad0f8670acdef9b6
aditya-chandraker <115742532+aditya-chandraker@users.noreply.github.com>
2024-07-30 20:06:35 -0400

    Update redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
-
 # author: Adi
 
-# Kill all existing tmux sessions
-tmux kill-server
+cd ~/fellowship_website
+git fetch
+git reset origin/main --hard
+source python3-virtualenv/bin/activate
+pip install -r requirements.txt
+sudo systemctl restart myporfolio
 
-# Start a new detached Tmux session named portfolio_website
-# cd, fetch, venv, install, run
-tmux new-session -d -s  portfolio_website \
-    "cd ~/fellowship_website && \
-    git fetch && \
-    git reset origin/main --hard && \
-    source python3-virtualenv/bin/activate && \
-    pip install -r requirements.txt && \
-    flask run --host=0.0.0.0"
-
-echo "Redeployment process initiated in tmux session. Your site should be up and running soon."
+echo "Redeployment process initiated in using systemd. Your site should be up and running soon."
